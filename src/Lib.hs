eb4f31ee0e0d9397c8e83768da5fb8d8bf30e99a
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-06-16 01:12:51 +1000

    add scripts

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
-{-# OverloadedStrings #-}
-
-import Network.HTTP.Client (httpLbs, newManager, parseRequest, responseBody)
+import Network.HTTP.Client (httpLbs, newManager, parseRequest, responseBody, requestHeaders)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.CaseInsensitive (CI)
+import qualified Data.ByteString as B
 
+userAgent :: (CI B.ByteString, B.ByteString)
+userAgent = ("User-Agent", "squeal")
 
 someFunc :: IO ()
 someFunc = do manager  <- newManager tlsManagerSettings
-              request  <- parseRequest "https://httpbin.org/get"
+              r1      <- parseRequest "https://haveibeenpwned.com/api/v2/breachedaccount/sanjsmailbox@gmail.com"
+              let request = r1 { requestHeaders = [userAgent]}
               response <- httpLbs request manager
               let body = responseBody response
               L8.putStrLn body
