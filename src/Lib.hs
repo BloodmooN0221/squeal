5c7ac068a97082b2ae3e55e820986aecb5ac7cca
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-06-16 22:54:12 +1000

    wip: breached accounts

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,6 +9,19 @@ import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.CaseInsensitive (CI)
 import qualified Data.ByteString as B
+import qualified Data.Text as T
+import Data.Aeson
+import Data.Aeson.Types (typeMismatch)
+
+data BreachedAccount = BreachedAccount { name :: !T.Text } deriving Show
+data BreachedAccounts = BreachedAccounts { accounts :: [BreachedAccount] } deriving Show
+
+instance FromJSON BreachedAccount where
+  parseJSON (Object v)  = BreachedAccount <$> v .: "Name"
+  parseJSON other = typeMismatch "BreachedAccount" other
+
+instance FromJSON BreachedAccounts where
+  parseJSON v = BreachedAccounts <$> (parseJSONList v)
 
 userAgent :: (CI B.ByteString, B.ByteString)
 userAgent = ("User-Agent", "squeal")
@@ -18,5 +31,7 @@ someFunc = do manager  <- newManager tlsManagerSettings
               r1      <- parseRequest "https://haveibeenpwned.com/api/v2/breachedaccount/sanjsmailbox@gmail.com"
               let request = r1 { requestHeaders = [userAgent]}
               response <- httpLbs request manager
-              let body = responseBody response
-              L8.putStrLn body
+              let response2 = responseBody response
+                  breachedAccounts = decode response2 :: Maybe BreachedAccounts
+                  result = maybe (T.pack "No breaches") (T.intercalate (T.pack ",") . fmap name . accounts) breachedAccounts
+              print result
