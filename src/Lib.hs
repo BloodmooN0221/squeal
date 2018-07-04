832ae42e00ed72e23d348943db75dc6382806f69
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-07-04 21:45:33 +1000

    added retry timeout to 429 error status

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,7 @@ import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.HTTP.Types.Status
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.CaseInsensitive (CI)
+import Data.List (find)
 import Control.Exception (tryJust)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C8
@@ -51,13 +52,22 @@ getBreaches (Email email) =
      responseE <- tryJust handleBreachesError $ httpLbs request manager
      return (responseBody <$> responseE)
 
-statusCodeToHttpError :: Status -> HttpError
-statusCodeToHttpError status
-  | status == badRequest400      = BadRequest "The account does not comply with an acceptable format (i.e. it's an empty string)"
-  | status == forbidden403       = Forbidden "No user agent has been specified in the request"
-  | status == notFound404        = NotFound "Not found — the account could not be found and has therefore not been pwned"
-  | status == tooManyRequests429 = TooManyRequests $ printf "Too many requests — the rate limit has been exceeded:%s" (C8.unpack $ statusMessage status)
-  | otherwise                    = OtherError (statusCode status) (statusMessage status)
+statusCodeToHttpError :: Response () -> HttpError
+statusCodeToHttpError response = getStatus (responseStatus response)
+  where
+    getStatus :: Status -> HttpError
+    getStatus status
+      | status == badRequest400      = BadRequest "The account does not comply with an acceptable format (i.e. it's an empty string)"
+      | status == forbidden403       = Forbidden "No user agent has been specified in the request"
+      | status == notFound404        = NotFound "Not found — the account could not be found and has therefore not been pwned"
+      | status == tooManyRequests429 = TooManyRequests $ printf "Too many requests — the rate limit has been exceeded. Try after: %s" (maybe "Unknown" (\b -> (C8.unpack b) M.<> " seconds") $ findHeader response "Retry-After")
+      | otherwise                    = OtherError (statusCode status) (statusMessage status)
+
+findHeader :: Response a -> CI B.ByteString -> Maybe B.ByteString
+findHeader response headerName =
+  let headers = responseHeaders response
+      maybeMatch = find (\(k, _) -> k == headerName) headers
+  in snd <$> maybeMatch
 
 getPasswordHashes :: Hash -> IO (Either PasswordHashError L8.ByteString)
 getPasswordHashes (Hash hashPrefix _) =
@@ -70,13 +80,13 @@ getPasswordHashes (Hash hashPrefix _) =
 
 -- TODO: Remove this duplication
 handleBreachesError :: HttpException -> Maybe BreachError
-handleBreachesError (HttpExceptionRequest _ (StatusCodeException response _)) = Just $ BreachApiError $ ApiCallError $ statusCodeToHttpError $ responseStatus response
+handleBreachesError (HttpExceptionRequest _ (StatusCodeException response _)) = Just $ BreachApiError $ ApiCallError $ statusCodeToHttpError response
 handleBreachesError (HttpExceptionRequest req context) = Just $ BreachApiError $ InvalidContext (RequestString $ show req) (ContextString $ show context)
 handleBreachesError (InvalidUrlException url reason)   = Just $ BreachApiError $ InvalidUrl (Url url) reason
 
 -- TODO: Remove this duplication
 handlePasswordHashError :: HttpException -> Maybe PasswordHashError
-handlePasswordHashError (HttpExceptionRequest _ (StatusCodeException response _)) = Just $ PasswordHashApiError $ ApiCallError $ statusCodeToHttpError $ responseStatus response
+handlePasswordHashError (HttpExceptionRequest _ (StatusCodeException response _)) = Just $ PasswordHashApiError $ ApiCallError $ statusCodeToHttpError response
 handlePasswordHashError (HttpExceptionRequest req context) = Just $ PasswordHashApiError $ InvalidContext (RequestString $ show req) (ContextString $ show context)
 handlePasswordHashError (InvalidUrlException url reason)   = Just $ PasswordHashApiError $ InvalidUrl (Url url) reason
 
