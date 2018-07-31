acfd713af9879e49c38e3e3af2061d4e663d0181
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-08-01 01:08:11 +1000

    use tar instead of zip for ghr

--- tools/install-ghr.sh
+++ tools/install-ghr.sh
@@ -12,9 +12,9 @@ else
     fi
   echo "Installing ghr for ${ARCH}"
   URL="https://github.com/tcnksm/ghr/releases/download/v0.10.2/ghr_v0.10.2_${ARCH}_386.tar.gz"
-  curl -L ${URL} > ghr.zip
+  curl -L ${URL} > ghr.tar.gz
   mkdir -p "$HOME/bin"
   export PATH="$HOME/bin:$PATH"
-  unzip ghr.zip -d "$HOME/bin"
-  rm ghr.zip
+  tar -xvzf ghr.zip -d "$HOME/bin"
+  rm ghr.tar.gz
 fi
