6152f454b35486f68cbf5aa95b9991ccf59ed0a3
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-08-01 00:42:48 +1000

    add osx build

--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -6,12 +6,6 @@ then
 else
   echo "Installing Stack for $TRAVIS_OS_NAME..."
   curl -sSL https://get.haskellstack.org/ | sh
-  # URL="https://www.stackage.org/stack/$TRAVIS_OS_NAME-x86_64"
-  # curl --location "$URL" > /tmp/stack.tar.gz
-  # tar -xvzf /tmp/stack.tar.gz --strip-components 1
-  # mkdir -p "$HOME/.local/bin"
-  # mv stack "$HOME/.local/bin/"
-  # rm /tmp/stack.tar.gz
 fi
 
 stack --version
