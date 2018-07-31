8fee0649082bba213c83224c3417bb897f8971e6
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-08-01 00:12:30 +1000

    try to fix script

--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -5,12 +5,13 @@ then
   echo 'Stack is already installed.'
 else
   echo "Installing Stack for $TRAVIS_OS_NAME..."
-  URL="https://www.stackage.org/stack/$TRAVIS_OS_NAME-x86_64"
-  curl --location "$URL" > /tmp/stack.tar.gz
-  tar -xvzf /tmp/stack.tar.gz --strip-components 1
-  mkdir -p "$HOME/.local/bin"
-  mv stack "$HOME/.local/bin/"
-  rm /tmp/stack.tar.gz
+  curl -sSL https://get.haskellstack.org/ | sh
+  # URL="https://www.stackage.org/stack/$TRAVIS_OS_NAME-x86_64"
+  # curl --location "$URL" > /tmp/stack.tar.gz
+  # tar -xvzf /tmp/stack.tar.gz --strip-components 1
+  # mkdir -p "$HOME/.local/bin"
+  # mv stack "$HOME/.local/bin/"
+  # rm /tmp/stack.tar.gz
 fi
 
 stack --version
